25e46040c8917cba0378447aa85452ca3df3a5ae
Stefan Zaruba <stefan.zaruba.dev@gmail.com>
2021-03-19 17:08:41 +0100

    Deep classifiers.

--- parser_secondary.py
+++ parser_secondary.py
@@ -4,7 +4,8 @@ nltk.download('punkt')
 import os
 from pathlib import Path
 import re
-
+from collections import OrderedDict
+import datetime as dt
 
 class ParserSecond:
     sentence_id = 1
@@ -15,6 +16,18 @@ class ParserSecond:
     input_file = None
     input_dir = None
     DELIMITER = '@@'
+    parties = ['SPÖ', 'ÖVP', 'FPÖ', 'Grüne', 'NEOS', 'ohne Klubzugehörigkeit']
+    speaker_party = {'Präsident': 'x', 'Schriftführ': 'x', 'Fürst': 'FPÖ', 'Schallenberg': 'ÖVP', 'Patek': 'x',
+                     'Kassegger': 'FPÖ', 'Heinisch-Hosek': 'SPÖ', 'Bogner-Strauß': 'ÖVP', 'Belakowitsch': 'FPÖ',
+                     'Müller': 'x', 'Krisper': 'NEOS', 'Hafenecker': 'FPÖ', 'Stilling': 'x', 'Kickl': 'FPÖ',
+                     'Kogler': 'Grüne', 'Meinl-Reisinger': 'NEOS', 'Amesbauer': 'FPÖ', 'Muchitsch': 'SPÖ',
+                     'Peschorn': 'x', 'Krainer': 'SPÖ', 'Silvan': 'SPÖ', 'Wöginger': 'ÖVP', 'Gahr': 'ÖVP',
+                     'Kurz': 'ÖVP', 'Blümel': 'ÖVP', 'Gewessler': 'Grüne', 'Laimer': 'SPÖ', 'Taschner': 'ÖVP',
+                     'Brückl': 'FPÖ', 'Blimlinger': 'Grüne', 'Brandstätter': 'NEOS', 'Lopatka': 'ÖVP',
+                     'Schellhorn': 'NEOS', 'Anschober': 'Grüne', 'Hammer': 'ÖVP', 'Raab': 'ÖVP', 'Köstinger': 'ÖVP',
+                     'Aschbacher': 'ÖVP', 'Alma': 'Grüne', 'Hauser': 'FPÖ', 'Tanner': 'ÖVP', 'Schramböck': 'ÖVP'}
+    party_governing = OrderedDict([('2020-01-07', ['ÖVP', 'Grüne']), ('2019-06-03', []), ('2019-05-28', ['ÖVP']),
+                                   ('2017-12-18', ['ÖVP', 'FPÖ'])])
 
     def __init__(self, input_dir, output_dir):
         self.sentence_id = self.determine_last_id() + 1
@@ -27,6 +40,7 @@ class ParserSecond:
         # files = ['06_2019_12_11.html']
         for file in sorted(files):
             self.process_file(file)
+        print("All files processed.")
 
 
     def process_file(self, file):
@@ -45,8 +59,8 @@ class ParserSecond:
 
         split = re.split('@@', line)
         speaker = split[0]
-        party = '_'
-        governing = '_'
+        party = self.determine_party(speaker)
+        governing = self.determine_governing(self.date, party)
         speech = split[1]
 
         speech = self.filter_remarks(speech)
@@ -58,7 +72,7 @@ class ParserSecond:
     def write_line(self, party, speaker, governing, sentence):
         output_file = 'all_sentences.csv'
         with open(self.output_dir + output_file, 'a', encoding="utf-8") as f:
-            line = str(self.sentence_id) + self.DELIMITER + self.date + self.DELIMITER + self.protocol_id + self.DELIMITER + party + self.DELIMITER + speaker + self.DELIMITER + governing + self.DELIMITER + sentence + '\n'
+            line = str(self.sentence_id) + self.DELIMITER + self.date + self.DELIMITER + self.protocol_id + self.DELIMITER + party + self.DELIMITER + speaker + self.DELIMITER + str(governing) + self.DELIMITER + sentence + '\n'
             print(line)
             f.write(line)
             f.flush()
@@ -68,6 +82,24 @@ class ParserSecond:
 
     def determine_last_id(self):
         return 1
+
+    def determine_party(self, speaker):
+        matches = re.findall('(?<=\\().+?(?=\\))', speaker)
+        if matches and matches[len(matches)-1] in self.parties:
+            return matches[len(matches)-1]
+        else:
+            for k in self.speaker_party.keys():
+                if re.search(k, speaker):
+                    return self.speaker_party[k]
+            self.speaker_party[speaker] = '_'
+            return '_'
+
+    def determine_governing(self, date_str, party):
+        date = dt.datetime.strptime(date_str, '%Y_%m_%d')
+        for k in self.party_governing:
+            if dt.datetime.fromisoformat(k) <= date:
+                return True if party in self.party_governing[k] else False
+        raise Exception('date outside scope')
 
 
 
---
+++ stats.py
@@ -0,0 +1,21 @@
+import re
+
+with open('protocols/labelled/only_lockdown.csv', 'r') as f:
+    col_names = ['sentiment', 'refs', 'lockdown', 'effects', 'impl/org', 'opinion']
+    opinions = ['+', '-', 'o', '?']
+    opinion_counts = {opinion: {col_name: {} for col_name in col_names} for opinion in opinions}
+    for opinion in opinions:
+        opinion_counts[opinion]['count'] = 0
+    f.readline()
+    for line in f:
+        col_vals = re.split('\\t', line)
+        opinion = col_vals[5]
+        opinion_counts[opinion]['count'] += 1
+        for i in range(len(col_names[:-1])):
+            col_val = col_vals[i]
+            col_name = col_names[i]
+            if not col_val in opinion_counts[opinion][col_name]:
+                opinion_counts[opinion][col_name][col_val] = 0
+            opinion_counts[opinion][col_name][col_val] += 1
+    print(opinion_counts)
+
